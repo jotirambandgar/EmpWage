00e1006d688354c92d7facd459bfac6a0889316a
BL-Jotiram <bandgar09@gmail.com>
2022-06-06 08:35:07 +0530

    Added all code

--- src/main/java/com/bridgelabz/EmpWage.java
+++ src/main/java/com/bridgelabz/EmpWage.java
@@ -3,5 +3,12 @@ package com.bridgelabz;
 public class EmpWage {
   public static void main(String[] args) {
     System.out.println("Welcome to employee wage");
+    EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+    boolean isEmpPresent = empWageBuilder.isEmpPresent();
+    if(isEmpPresent){
+      System.out.println("Employee is present");
+    }else {
+      System.out.println("Employee is absent");
+    }
   }
 }
--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -1,4 +1,11 @@
 package com.bridgelabz;
 
 public class EmpWageBuilder {
+  public boolean isEmpPresent(){
+    double random = Math.random();
+    if(random < 0.5){
+      return false;
+    }
+    return true;
+  }
 }
